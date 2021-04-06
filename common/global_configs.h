0a8f73512d50a4f7c71da50a187f5403f902f889
Mihai Bujanca <mihaibujanca@github.com>
2021-04-06 13:20:47 +0200

    Revert boundary clip change

--- common/global_configs.h
+++ common/global_configs.h
@@ -10,12 +10,12 @@
 
 //Clip pixels at boundary for better windowed search. 
 //Please refer to clip.md under imgproc directory for details
-//#ifndef boundary_clip
-//#define boundary_clip 20
-//#endif
 #ifndef boundary_clip
-#define boundary_clip 0
+#define boundary_clip 20
 #endif
+//#ifndef boundary_clip
+//#define boundary_clip 0
+//#endif
 
 
 //For pcl access of new in debug mode
